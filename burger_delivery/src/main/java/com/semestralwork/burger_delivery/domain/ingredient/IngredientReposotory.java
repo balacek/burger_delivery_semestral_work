15e8e7183e8ddeeac9eace4df51e8b7c9cc8419c
balacek <david.bali@seznam.cz>
2020-12-26 10:09:58 +0100

    adding repositories

---
+++ burger_delivery/src/main/java/com/semestralwork/burger_delivery/domain/ingredient/IngredientReposotory.java
@@ -0,0 +1,6 @@
+package com.semestralwork.burger_delivery.domain.ingredient;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientReposotory extends JpaRepository<Ingredient, Long> {
+}
