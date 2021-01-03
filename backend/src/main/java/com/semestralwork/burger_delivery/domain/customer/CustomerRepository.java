b56848bcde8f08f16f8b45b5a35f8ee62cfbb432
balacek <david.bali@seznam.cz>
2021-01-03 12:19:17 +0100

    repo bug

--- backend/src/main/java/com/semestralwork/burger_delivery/domain/customer/CustomerRepository.java
+++ backend/src/main/java/com/semestralwork/burger_delivery/domain/customer/CustomerRepository.java
@@ -9,6 +9,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query(value = "SELECT * FROM CUSTOMER WHERE customer.email = ?1", nativeQuery = true)
     Optional<Customer> findCustomerByEmail(String email);
 
-    @Query(value = "SELECT * FROM CUSTOMER WHERE customer.id = ?1 AND customer.email = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM CUSTOMER WHERE customer_id = ?1 AND email = ?2", nativeQuery = true)
     Optional<Customer> getCustomerByPhoneAndEmail(Long id, String email);
 }
